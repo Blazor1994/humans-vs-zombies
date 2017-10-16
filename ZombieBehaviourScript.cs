341bf5012cd3add53de27bba1b0d0e653e77b39b
Gavin <gvnprtr3@googlemail.com>
2017-10-16 21:14:27 +0100

    Commit - Humans Vs Zombies Basic Test: Simple Chase
    Just my current assets, very incomplete at this moment. 
    Currently, its just a change of speed using moveTowards as a minus speed towards the target, thus making the object move away from the 
    target.

---
+++ ZombieBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieBehaviourScript : MonoBehaviour {
+
+    float speed = 0.01f;
+    //transform for human which is target
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        transform.LookAt(target.position);
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
+     
+    }
+}
---
+++ humanBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class humanBehaviourScript : MonoBehaviour
+{
+    float speed = -0.02f;
+    //transform for zombie which is target
+    public Transform target;
+    // Use this for initialization
+    void Start(){
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+   
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
+
+    }
+}
