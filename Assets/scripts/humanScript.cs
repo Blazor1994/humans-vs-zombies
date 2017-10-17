104d77509c2a1ba9eaf6e78d6f54e4ddec3ad632
Blase Anderson <blazor1994@gmail.com>
2017-10-17 23:11:52 +0100

    starting to script, simple ai to human and zombie

---
+++ Assets/scripts/humanScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class humanScript : MonoBehaviour {
+
+	// Use this for initialization
+
+	Transform tr_Zombie;
+	float f_RotSpeed = 3.0f, f_MoveSpeed = 1.0f;
+
+	void Start () {
+			tr_Zombie = GameObject.FindGameObjectWithTag("Zombie").transform;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log(transform.position);
+		transform.rotation = Quaternion.Slerp(transform.rotation, 
+												Quaternion.LookRotation(tr_Zombie.position - transform.position),
+												f_RotSpeed * Time.deltaTime);
+		
+		transform.position -= transform.forward*f_MoveSpeed*Time.deltaTime;
+	}
+}
---
+++ Assets/scripts/zombieScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombieScript : MonoBehaviour {
+
+	// Use this for initialization
+
+	Transform tr_Player;
+	float f_RotSpeed = 3.0f, f_MoveSpeed = 0.5f;
+
+
+	void Start () {
+			tr_Player = GameObject.FindGameObjectWithTag("Human").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.rotation = Quaternion.Slerp(transform.rotation, 
+												Quaternion.LookRotation(tr_Player.position - transform.position),
+												f_RotSpeed * Time.deltaTime);
+		
+		transform.position += transform.forward*f_MoveSpeed*Time.deltaTime;
+	}
+}
