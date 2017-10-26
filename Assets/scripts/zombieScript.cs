122a52ae25aae590dfbc2c14e7aa627080a68e38
James Oliver <james@jamesjo.co.uk>
2017-10-26 13:17:31 +0100

    Updated Scene and Zombie Script
    Created basic test scene for Zombie AI testing. Basic functionality is present - i.e the Zombie chases the Human when in range and also chooses the closest of the two.

--- Assets/scripts/zombieScript.cs
+++ Assets/scripts/zombieScript.cs
@@ -1,14 +1,24 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ZombieScript : MonoBehaviour {
 
-    float zombieSpeedNormal = 1f;
-	// Use this for initialization
-	void Start () {
-		
-	}
+    float speed = 1f;
+
+    // Update is called once per frame
+    void Update() {
+        //get humans distance from zombie
+        var humanDistance = Vector3.Distance(transform.position, FindClosestEnemy().transform.position);
+        //if the humans distance is less than 15meters
+        if (humanDistance < 17) {
+            transform.LookAt(FindClosestEnemy().transform.position);
+            //For documentation on Time.deltaTime https://docs.unity3d.com/ScriptReference/Time-deltaTime.html
+            transform.position = Vector3.MoveTowards(transform.position, FindClosestEnemy().transform.position, 1 * speed * Time.deltaTime);
+
+        }
+    }
+    // TODO: Move this to a seperate Utility Class.
     //Reference: https://docs.unity3d.com/ScriptReference/GameObject.FindGameObjectsWithTag.html
     public GameObject FindClosestEnemy()
     {
@@ -19,27 +29,14 @@ public class ZombieScript : MonoBehaviour {
         Vector3 position = transform.position;
         foreach (GameObject go in gos)
         {
-            Vector3 diff = go.transform.position - position;
-            float curDistance = diff.sqrMagnitude;
-            if (curDistance < distance)
+            Vector3 difference = go.transform.position - position;
+            float currentDistance = difference.sqrMagnitude;
+            if (currentDistance < distance)
             {
                 closest = go;
-                distance = curDistance;
+                distance = currentDistance;
             }
         }
         return closest;
-    }
-    // Update is called once per frame
-    void Update() {
-        //get humans distance from zombie
-        var distance = Vector3.Distance(transform.position, FindClosestEnemy().transform.position);
-        //if the humans distance is less than 15meters
-        if(distance < 17)
-        {
-            transform.LookAt(FindClosestEnemy().transform.position);
-            //For documentation on Time.deltaTime https://docs.unity3d.com/ScriptReference/Time-deltaTime.html
-            transform.position = Vector3.MoveTowards(transform.position, FindClosestEnemy().transform.position, 1 * zombieSpeedNormal * Time.deltaTime);
-
-        }
     }
 }
