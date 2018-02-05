86d034285de5b36b8446630cea7c65e1efee2911
Blase Anderson <blazor1994@gmail.com>
2018-02-05 19:01:37 +0000

    post processing, houses -> cars, menu

---
+++ Assets/scripts/menuScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuScript : MonoBehaviour {
+
+	public float cellCount = 10.0f;
+	public float carCount = 0.0f;
+	public float humanCount = 0.0f; 
+	public float zombieCount = 0.0f;
+
+	public UnityEngine.UI.Slider cellSlider;
+	public UnityEngine.UI.Slider carSlider;
+	public UnityEngine.UI.Slider humanSlider;
+	public UnityEngine.UI.Slider zombieSlider;
+
+	public UnityEngine.UI.Text cellText;
+	public UnityEngine.UI.Text carText;
+	public UnityEngine.UI.Text humanText;
+	public UnityEngine.UI.Text zombieText;
+
+	public GameManager gameManager;
+
+	public void sliderChange(string type)
+	{
+		switch(type)
+		{
+			case "cell": 
+				cellCount = cellSlider.value;
+				cellText.text = cellSlider.value.ToString();
+				break;
+			case "car": 
+				carCount = carSlider.value;
+				carText.text = carSlider.value.ToString();
+				break;
+			case "human": 
+				humanCount = humanSlider.value;
+				humanText.text = humanSlider.value.ToString();
+				break;
+			case "zombie": 
+				zombieCount = zombieSlider.value;
+				zombieText.text = zombieSlider.value.ToString();
+				break;
+			
+		}
+	}
+
+	public void startButton()
+	{
+		gameManager.BeginGame(cellSlider.value, carSlider.value, humanSlider.value, zombieSlider.value);
+	}
+
+	public void randomButton()
+	{
+		cellSlider.value = Random.Range(cellSlider.minValue, cellSlider.maxValue);
+		carSlider.value = Random.Range(carSlider.minValue, carSlider.maxValue);
+		humanSlider.value = Random.Range(humanSlider.minValue, humanSlider.maxValue);
+		zombieSlider.value = Random.Range(zombieSlider.minValue, zombieSlider.maxValue);
+		
+	}
+	// Use this for initialization
+	void Start () {
+		//cellSlider = GameObject.FindGameObjectWithTag("cellSlider");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		carSlider.maxValue = cellSlider.value/2f;
+		zombieSlider.maxValue = cellSlider.value/2;
+		humanSlider.maxValue = cellSlider.value/2;
+		
+	}
+}
---
+++ Assets/scripts/roverScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roverScript : MonoBehaviour {
+
+	// Use this for initialization
+
+	GameObject map;
+	void Start () {
+
+		map = GameObject.FindGameObjectWithTag("map");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+
+	 private void OnCollisionEnter (Collision collision) {
+		 //Debug.Log("Collision");
+	if (collision.gameObject.tag == "Wall") {
+				Debug.Log ("Removing house");
+				Destroy(collision.gameObject);
+				//map.GetComponent<Map>().refreshNavMesh();
+
+				
+				}
+	else if(collision.gameObject.tag == "Finish")
+	{
+		Debug.Log("Rover finished!");
+		//GetComponent<NavMeshAgent>().enabled = true;
+		GetComponent<NavMeshAgentScript>().enabled = false;
+		GetComponent<roverScript>().enabled = false;
+		Destroy(gameObject);
+		map.GetComponent<Map>().generateCharacters();
+	}
+
+	}
+}
