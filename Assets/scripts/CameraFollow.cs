d17baaa3969b61a3027dd8e5ea23e00322a0f633
Gavin <gvnprtr3@googlemail.com>
2018-02-16 14:43:19 +0000

    camera now follows the human closest to the final objective, humans not responding using the raycast to find zombies to back away from at this moment. Solution is close

--- Assets/scripts/CameraFollow.cs
+++ Assets/scripts/CameraFollow.cs
@@ -4,32 +4,20 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
 public float speed = 0.2f;
 public Vector3 offset;
-public GameObject target;
+public GameObject target = null;
 
-GameObject FindClosestEnemy() {
-        GameObject[] gos;
-        gos = GameObject.FindGameObjectsWithTag("Human");
-		GameObject closest = null;
-        float distance = Mathf.Infinity;
-        Vector3 position = transform.position;
-        foreach (GameObject go in gos) {
-            Vector3 diff = go.transform.position - position;
-            float curDistance = diff.sqrMagnitude;
-            if (curDistance < distance) {
-                closest = go;
-                distance = curDistance;
-            }
-        }
-
-		return closest;
-    }
 void LateUpdate(){
 
             
     if(target == null){
-        	transform.position = FindClosestEnemy().transform.position + offset;
+        	
+    }
+    if(FindClosestHumanShop.hum != null){
+         transform.position = FindClosestHumanShop.hum.transform.position + offset;
+            target = FindClosestHumanShop.hum;
              transform.LookAt(target.transform);
     }
+
    
 	
 }
